11d0c930ca39f83e0e83b885bf1fb7dd23a5fdad
JONGHEON LEE <ika7204@naver.com>
2022-08-03 23:38:56 +0900

    [UPD]Uploading Fastapi + Vercel Template

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # fastapi_vercel_template
 For Fastapi + Vercel Stack
+
+Start with python3 main.py
+
+Official Document
+https://fastapi.tiangolo.com/ko/
+
+Reference
+https://blog.logrocket.com/deploying-fastapi-applications-to-vercel/
---
+++ server/api.py
@@ -0,0 +1,39 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+
+import requests
+#########################################################
+app = FastAPI()
+origins = ["*"]
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+#위의 설정은 모든 origin, 모든 cookie, 모든 method, 모든 header를 allow한다.
+########################POST를 위한 모델 설정#############################
+class test(BaseModel):
+    test: str
+    test: str
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+@app.get("/", tags=["Root"])
+async def read_root():
+  return { 
+    "message": "Welcome to my Project by JONGHEON LEE"
+   }
+   
+@app.post("/test")
+async def TEST(item: test):
+    params = dict(item)
+    return params
